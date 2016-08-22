2833f492d387c16a330e2e34a02de98dccc02f4b
Talayruk <notejde@gmail.com>
2016-08-21 22:31:12 -0700

    added $ console.log inputN

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var pingPong = function(inputN) {
+  var result = [];
+  for (var n = 1; n === inputN; n++) {
+    result.push(n);
+  }
+  return result;
+}
+
+
+$(document).ready(function() {
+  $(".ping-pong .btn" ).click(function(event) {
+    var inputN = (parseInt$("inputNumber").val());
+    console.log(inputN);
+    // if (inputN >= 1) {
+    //   var output = pingPong(inputN)
+    // }
+    event.preventDefault();
+  }
+});
