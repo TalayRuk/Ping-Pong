fe813a7ad0c641a3158d7eea0b756ba3e385bc5e
Talayruk <notejde@gmail.com>
2016-08-22 02:25:19 -0700

    fixed pingPong function changed n < inputN .. append the output correctly but not in the list type

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pingPong = function(inputN) {
   var result = [];
-  for (var n = 1; n == inputN; n++) {
+  for (var n = 1; n < inputN; n++) {
     result.push(n);
   }
   return result;
@@ -12,14 +12,13 @@ $(document).ready(function() {
   $(".ping-pong" ).submit(function(event) {
     event.preventDefault();
     var inputN = parseInt($("#inputNumber").val());
-    if (inputN >= 1) {
-      var output = pingPong(inputN);
+    var output = pingPong(inputN);
         // for (var i = 1; i <= parseInt("#inputNumber"); i++) {
-        $(".showoutput").text("<li>" + output + "</li>");
-        // }
-    } else {
-      $(".showoutput").text("Please enter only number more than 1!")
-    }
+        $(".showoutput").append("<li>" + output + "</li>");
+    //     // }
+    // } else {
+    //   $(".showoutput").text("Please enter only number more than 1!")
+    // }
 
 
   });
