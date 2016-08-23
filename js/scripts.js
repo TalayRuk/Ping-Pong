54d518923d6a5448cfd821c33197366b6d7521ab
Talayruk <notejde@gmail.com>
2016-08-23 10:58:23 -0700

    moved event.prevent back to under submit - moved to bottom-not working:

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 // business logic
 var pingPong = function(inputN) {
-  var result = "";
+  var result = [];
   for (var i = 0; i <= inputN; i++) {
     if (i % 15 === 0) {
-      result = result + "PING-PONG";
+      result = "PING-PONG";
     } else if (i % 5 === 0) {
       result = "PONG";
     } else if (i % 3 === 0) {
       result = "PING";
     } else {
-      result =
+      result = result.push(i);
     }
   }
+  return result;
 }
 // user logic
 $(document).ready(function() {
