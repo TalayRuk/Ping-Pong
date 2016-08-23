bd240dc6ae21065ae4a50e40dc7d9dea6128be76
Talayruk <notejde@gmail.com>
2016-08-23 07:58:12 -0700

    added i%3

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,10 @@ var pingPong = function(inputN) {
   for (var i = 1; i <= inputN; i++) {
     if (i % 15 === 0) {
       result = "PING-PONG";
-    
+    } else if (i % 5 === 0) {
+      result = "PONG";
+    } else if (i % 3 === 0) {
+      result = "PING";
+    }
+  }
+}
