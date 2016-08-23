b7eb357a14c55d3e2721dc60e8d8e9af2ca7e6f9
Talayruk <notejde@gmail.com>
2016-08-23 14:30:22 -0700

    empty() works needed to be under submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,16 @@
 // business logic
 var pingPong = function(inputN) {
   var result = "";
-  // for (var i =1; i <= inputN; i++ ) {
-  //
-  //   if (i % 3 === 0 && i % 15 !== 0) {
-  //     $(".showoutput").append("<li>" + "PING" + "</li>");
-  //
-  //   } else if (i % 5 === 0 && i % 15 !== 0) {
-  //     $(".showoutput").append("<li>" + "PONG" + "</li>");
-  //
-  //   } else if (i % 15 === 0) {
-  //     $(".showoutput").append("<li>" + "PING-PING" + "</li>");
-  //
-  //   } else {
-  //     $(".showoutput").append("<li>" + i + "</li>");
-  //     console.log(i);
-  //   }
-    //
     return result;
-// }
+};
 
-}
-
-
-// user logic
+// user interface
 $(function() {
   $(".ping-pong" ).submit(function(event) {
     event.preventDefault();
+    $("ul").empty();
     var inputN = parseInt($("#inputNumber").val());
-    console.log(inputN);
-      var output = pingPong(inputN);
+    var output = pingPong(inputN);
       for (var i =1; i <= inputN; i++ ) {
 
         if (i % 3 === 0 && i % 15 !== 0) {
@@ -43,14 +24,8 @@ $(function() {
 
         } else {
           $("ul.showOutput").append("<li>" + i + "</li>");
-          console.log(i);
+
         }
-
-    }
-
-  $("ul").detach();
-  //     for (var i =1; i <= inputN; i++ ) {
-  //       // $(".showoutput").append("<li>" + output + "</li>");
-  //     }
+      }
   });
 });
