356c98f9e684a1d8bf800dc939c843b3b41ee60a
Talayruk <notejde@gmail.com>
2016-08-23 12:42:59 -0700

    moved 15 to top ..ping-pong still show up first

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var pingPong = function(inputN) {
   var result = "";
   for (var i = 0; i <= inputN; i++ ) {
 
+   if (i % 15 === 0) {
+    $(".showoutput").append("<li>" + "PING-PONG" + "</li>");
+
     if (i % 3 === 0 && i % 15 != 0) {
-      $(".showoutput").append("<li>" + "PING" + "</li>");
+      result = result.replace(i, "PING");
 
     } else if (i % 5 === 0 && i % 15 != 0) {
       $(".showoutput").append("<li>" + "PONG" + "</li>");
-
-    } else if (i % 15 === 0) {
-      $(".showoutput").append("<li>" + "PING-PING" + "</li>");
 
     } else {
       $(".showoutput").append("<li>" + i + "</li>");
