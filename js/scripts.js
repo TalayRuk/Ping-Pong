65895b5af92fc206d770f9b024048cd326c64549
Talayruk <notejde@gmail.com>
2016-08-23 07:55:47 -0700

    added business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,8 @@
+// business logic
 var pingPong = function(inputN) {
   var result = "";
-  if ()
-
-  for (var n = 1; n < inputN; n++) {
-    result.push(n);
-  }
-  return result;
-
-}
-
-
-$(document).ready(function() {
-  $(".ping-pong" ).submit(function(event) {
-    event.preventDefault();
-    var inputN = parseInt($("#inputNumber").val());
-    var output = pingPong(inputN);
-        // for (var i = 1; i <= parseInt("#inputNumber"); i++) {
-        $(".showoutput").append("<li>" + output + "</li>");
-    //     // }
-    // } else {
-    //   $(".showoutput").text("Please enter only number more than 1!")
-    // }
-
-
-  });
-});
+  var i;
+  for (var i = 1; i <= inputN; i++) {
+    if (i % 15 === 0) {
+      result = "PING-PONG";
+    
