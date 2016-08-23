b1f8c4b85c63b5a577202abbb3ba7bf0bf78987e
Talayruk <notejde@gmail.com>
2016-08-23 10:53:08 -0700

    added result = result + ping

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 // business logic
 var pingPong = function(inputN) {
-  var result = [];
-  for (var i = 1; i <= inputN; i++) {
+  var result = "";
+  for (var i = 0; i <= inputN; i++) {
     if (i % 15 === 0) {
-      result + "PING-PONG";
+      result = result + "PING-PONG";
     } else if (i % 5 === 0) {
       result = "PONG";
     } else if (i % 3 === 0) {
       result = "PING";
     } else {
-       result.push[i]
+      result =
     }
   }
 }
 // user logic
 $(document).ready(function() {
   $("form.ping-pong" ).submit(function(event) {
-    event.preventDefault();
     var inputN = parseInt($("#inputNumber").val());
     console.log(inputN);
     var output = pingPong(inputN);
     $(".showoutput").append("<li>" + output + "</li>");
 
+    event.preventDefault();
   });
 });
