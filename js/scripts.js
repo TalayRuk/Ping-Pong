599d68cf3b52af0688362ea5c4a7a185a7ae527a
Talayruk <notejde@gmail.com>
2016-08-25 00:50:32 -0700

    fixed spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,19 @@
 // business logic
 var pingPong = function(inputN) {
   var result = [];
-
-
   for (var i = 0; i <= inputN; i++ ) {
-
     if (i % 3 === 0 && i % 15 !== 0) {
       result.push("PING");
     } else if (i % 5 === 0 && i % 15 !== 0) {
       result.push("PONG");
-
     } else if (i % 15 === 0) {
       result.push("PING-PONG");
-
     } else {
       result.push(i);
     }
   }
   return result;
 };
-
 // user interface
 $(function() {
   $(".ping-pong" ).submit(function(event) {
@@ -32,9 +26,7 @@ $(function() {
     for (var i = 1; i < inputN; i++) {
      $("#showOutput").append("<li>" + output[i] + "</li>");
    }
-
   });
-
   $("#clickRules").click(function() {
     $(".Rules").show();
   });
